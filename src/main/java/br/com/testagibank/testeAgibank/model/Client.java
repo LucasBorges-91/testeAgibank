da016086cda3c473de322287c79af0731a0f7497
Lucas Borges <lucas.pborges@yahoo.com.br>
2020-07-24 19:19:59 -0300

    create client class

---
+++ src/main/java/br/com/testagibank/testeAgibank/model/Client.java
@@ -0,0 +1,64 @@
+package br.com.testagibank.testeAgibank.model;
+
+import org.springframework.data.annotation.Id;
+import org.springframework.data.mongodb.core.mapping.Document;
+
+@Document( collection = "client")
+public class Client {
+
+    @Id
+    private Integer id;
+    private String cnpj;
+    private String name;
+    private String businessArea;
+
+    public Client() {}
+
+    public Client(Integer id, String cnpj, String name, String businessArea) {
+        this.id = id;
+        this.cnpj = cnpj;
+        this.name = name;
+        this.businessArea = businessArea;
+    }
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getCnpj() {
+        return cnpj;
+    }
+
+    public void setCnpj(String cnpj) {
+        this.cnpj = cnpj;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getBusinessArea() {
+        return businessArea;
+    }
+
+    public void setBusinessArea(String businessArea) {
+        this.businessArea = businessArea;
+    }
+
+    @Override
+    public String toString() {
+        return "Client{" +
+                "cnpj='" + cnpj + '\'' +
+                ", name='" + name + '\'' +
+                ", businessArea='" + businessArea + '\'' +
+                '}';
+    }
+}
