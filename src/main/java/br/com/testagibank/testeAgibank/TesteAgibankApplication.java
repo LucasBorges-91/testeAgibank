7f3e178119b113d1f713b9ea8811a00b313773be
Lucas Borges <lucas.pborges@yahoo.com.br>
2020-07-24 19:08:46 -0300

    create project

---
+++ src/main/java/br/com/testagibank/testeAgibank/TesteAgibankApplication.java
@@ -0,0 +1,13 @@
+package br.com.testagibank.testeAgibank;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TesteAgibankApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TesteAgibankApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/testagibank/testeAgibank/TesteAgibankApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.testagibank.testeAgibank;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TesteAgibankApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
