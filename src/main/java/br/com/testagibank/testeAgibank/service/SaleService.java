202ee17180bf5f3c07d3d92d0428f38fe4ea9aa8
Lucas Borges <lucas.pborges@yahoo.com.br>
2020-07-26 15:42:37 -0300

    add most expansive sale method

--- src/main/java/br/com/testagibank/testeAgibank/service/SaleService.java
+++ src/main/java/br/com/testagibank/testeAgibank/service/SaleService.java
@@ -1,7 +1,9 @@
 package br.com.testagibank.testeAgibank.service;
 
 import br.com.testagibank.testeAgibank.model.FileDat;
+import br.com.testagibank.testeAgibank.model.Item;
 import br.com.testagibank.testeAgibank.model.Sale;
+import br.com.testagibank.testeAgibank.model.Seller;
 import br.com.testagibank.testeAgibank.repository.SaleRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
@@ -16,8 +18,30 @@ public class SaleService {
     public Boolean save( FileDat fileDat ) {
         List<Sale> sales = fileDat.getSales();
         sales.stream()
-                .filter( sale -> sale.getId() != null && sale.getItem() != null && sale.getSalesmanName() != "" )
+                .filter( sale -> sale.getId() != null && sale.getItens() != null && sale.getSalesmanName() != "" )
                 .forEach( sale -> repository.save( sale ) );
         return true;
     }
+
+    public Integer mostExpansiveSale( List<Sale> sales ) {
+        Double saleExpansiveValue = 0.0;
+        Integer saleExpansiveId = 0;
+
+        for ( Sale sale: sales ) {
+            List<Item> itens = sale.getItens();
+            for ( Item item: itens ) {
+                double subtotal =+ item.getPrice() * item.getQuantity();
+                if ( subtotal > saleExpansiveValue ) {
+                    saleExpansiveValue = subtotal;
+                    saleExpansiveId = sale.getId();
+                }
+            }
+        }
+        return saleExpansiveId;
+    }
+
+//    public String worstSeller( List<Sale> sales, List<Seller> sellers ) {
+//
+//    }
+
 }
