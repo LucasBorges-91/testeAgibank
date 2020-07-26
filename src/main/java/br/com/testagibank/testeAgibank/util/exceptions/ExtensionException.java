d7ea0a9035e4494341ae03ee4664f18f7fa09d5c
Lucas Borges <lucas.pborges@yahoo.com.br>
2020-07-26 13:18:47 -0300

    create exception to validade formate file

---
+++ src/main/java/br/com/testagibank/testeAgibank/util/exceptions/ExtensionException.java
@@ -0,0 +1,6 @@
+package br.com.testagibank.testeAgibank.util.exceptions;
+
+public class ExtensionException extends RuntimeException {
+
+    public ExtensionException( String msg ) { super( msg ); }
+}
