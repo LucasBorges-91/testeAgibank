988a50616f9d04a3453416e35fc3cb41ee7dc981
Lucas Borges <lucas.pborges@yahoo.com.br>
2020-07-26 13:01:37 -0300

    create seller repository

---
+++ src/main/java/br/com/testagibank/testeAgibank/repository/SellerRepository.java
@@ -0,0 +1,9 @@
+package br.com.testagibank.testeAgibank.repository;
+
+import br.com.testagibank.testeAgibank.model.Seller;
+import org.springframework.data.mongodb.repository.MongoRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface SellerRepository extends MongoRepository<Seller, String> {
+}
