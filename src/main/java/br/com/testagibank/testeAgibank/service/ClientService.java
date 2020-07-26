4790605bbaf642f67a0d85f0e3c537e09ef492cb
Lucas Borges <lucas.pborges@yahoo.com.br>
2020-07-26 13:06:58 -0300

    create client service

---
+++ src/main/java/br/com/testagibank/testeAgibank/service/ClientService.java
@@ -0,0 +1,24 @@
+package br.com.testagibank.testeAgibank.service;
+
+import br.com.testagibank.testeAgibank.model.Client;
+import br.com.testagibank.testeAgibank.model.FileDat;
+import br.com.testagibank.testeAgibank.repository.ClientRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+public class ClientService {
+
+    @Autowired
+    private ClientRepository repository;
+
+    public Boolean save( FileDat fileDat ) {
+        List<Client> clients = fileDat.getClients();
+        clients.stream()
+                .filter( client -> client.getCnpj().length() == 16 && client.getName() != "" && client.getBusinessArea() != "" )
+                .forEach( client -> repository.save( client ) );
+        return true;
+    }
+}
