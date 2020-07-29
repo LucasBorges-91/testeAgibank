fa946ed44daacd7db76a908ebd96720a33466023
Lucas Borges <lucas.pborges@yahoo.com.br>
2020-07-29 20:33:51 -0300

    create unit test client

---
+++ src/test/java/br/com/testagibank/testeAgibank/model/ClientTest.java
@@ -0,0 +1,41 @@
+package br.com.testagibank.testeAgibank.model;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+class ClientTest {
+
+    @Test
+    void getId() {
+        Client client = new Client();
+        client.setId( "12345" );
+
+        Assertions.assertEquals( "12345", client.getId() );
+    }
+
+    @Test
+    void getCnpj() {
+        Client client = new Client();
+        client.setCnpj( "1234567891234567" );
+
+        Assertions.assertEquals( "1234567891234567", client.getCnpj() );
+    }
+
+    @Test
+    void getName() {
+        Client client = new Client();
+        client.setName( "Lucas" );
+
+        Assertions.assertEquals( "Lucas", client.getName() );
+    }
+
+    @Test
+    void getBusinessArea() {
+        Client client = new Client();
+        client.setBusinessArea( "Rural" );
+
+        Assertions.assertEquals( "Rural", client.getBusinessArea() );
+    }
+
+
+}
