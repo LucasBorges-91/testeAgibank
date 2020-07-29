bd5807654096b63cc062631e8447dd1804526cf8
Lucas Borges <lucas.pborges@yahoo.com.br>
2020-07-28 21:55:06 -0300

    add findByName and findByCpf methods

--- src/main/java/br/com/testagibank/testeAgibank/repository/SellerRepository.java
+++ src/main/java/br/com/testagibank/testeAgibank/repository/SellerRepository.java
@@ -6,4 +6,7 @@ import org.springframework.stereotype.Repository;
 
 @Repository
 public interface SellerRepository extends MongoRepository<Seller, String> {
+
+    Seller findByName( String name );
+    Seller findByCpf( String cpf );
 }
