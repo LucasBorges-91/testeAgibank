306113d3889902dc16dc08238d43a64ed6adf9f1
Lucas Borges <lucas.pborges@yahoo.com.br>
2020-07-28 22:03:58 -0300

    create itemIdCheck and add to validate save, create map in save to add database just id and price of itens

--- src/main/java/br/com/testagibank/testeAgibank/service/ItemService.java
+++ src/main/java/br/com/testagibank/testeAgibank/service/ItemService.java
@@ -6,7 +6,11 @@ import br.com.testagibank.testeAgibank.repository.ItemRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import java.util.ArrayList;
 import java.util.List;
+import java.util.Optional;
+import java.util.stream.Collectors;
+
 @Service
 public class ItemService {
 
@@ -15,9 +19,19 @@ public class ItemService {
 
     public Boolean save( FileDat fileDat ) {
         List<Item> itens = fileDat.getItens();
+
         itens.stream()
                 .filter( item -> item.getId() != null && item.getQuantity() > 0 && item.getPrice() > 0 )
+                .filter( item -> !itemIdCheck( item.getId() ) )
+                .map( item ->  new Item( item.getId(), item.getPrice() ) )
                 .forEach( item -> repository.save( item ) );
+
         return true;
+    }
+
+    public Boolean itemIdCheck( Integer id ) {
+        Optional<Item> item = repository.findById( id );
+
+        return item.isEmpty() ? false : true;
     }
 }
