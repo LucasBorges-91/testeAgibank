ae4117bff20b2550d3cb34d3f93105f6a7642060
Lucas Borges <lucas.pborges@yahoo.com.br>
2020-07-28 21:53:08 -0300

    add method findByCpf

--- src/main/java/br/com/testagibank/testeAgibank/repository/ClientRepository.java
+++ src/main/java/br/com/testagibank/testeAgibank/repository/ClientRepository.java
@@ -4,6 +4,10 @@ import br.com.testagibank.testeAgibank.model.Client;
 import org.springframework.data.mongodb.repository.MongoRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
+
 @Repository
 public interface ClientRepository extends MongoRepository<Client, String> {
+
+    Client findByCnpj( String cnpj );
 }
