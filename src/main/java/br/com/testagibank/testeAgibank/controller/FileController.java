665e703f281f81fb644e24381208a465d104a3ce
Lucas Borges <lucas.pborges@yahoo.com.br>
2020-08-03 19:58:21 -0300

    remove annotations mongo database and fix call methods

--- src/main/java/br/com/testagibank/testeAgibank/controller/FileController.java
+++ src/main/java/br/com/testagibank/testeAgibank/controller/FileController.java
@@ -1,58 +1,25 @@
 package br.com.testagibank.testeAgibank.controller;
 
 import br.com.testagibank.testeAgibank.model.FileDat;
-import br.com.testagibank.testeAgibank.model.Seller;
 import br.com.testagibank.testeAgibank.service.ClientService;
-import br.com.testagibank.testeAgibank.service.ItemService;
 import br.com.testagibank.testeAgibank.service.SaleService;
 import br.com.testagibank.testeAgibank.service.SellerService;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.stereotype.Controller;
 
-@Controller
 public class FileController {
 
-    @Autowired
-    private SellerService serviceSeller;
-
-    @Autowired
-    private ClientService serviceClient;
-
-    @Autowired
-    private SaleService serviceSale;
-
-    @Autowired
-    private ItemService serviceItem;
-
-    public Boolean saveSeller( FileDat fileDat ) {
-        return serviceSeller.save( fileDat );
-    }
-
-    public Boolean saveClient( FileDat fileDat ) {
-        return serviceClient.save( fileDat );
-    }
-
-    public Boolean saveSale( FileDat fileDat ) {
-        return serviceSale.save( fileDat );
-    }
-
-    public Boolean saveItem( FileDat fileDat ) {
-        return serviceItem.save( fileDat );
-    }
-
     public Integer amountClients( FileDat fileDat ) {
-        return serviceClient.amountClients( fileDat.getClients() );
+        return ClientService.amountClients( fileDat.getClients() );
     }
 
     public Integer amountSellers( FileDat fileDat ) {
-        return serviceSeller.amountSellers( fileDat.getSellers() );
+        return SellerService.amountSellers( fileDat.getSellers() );
     }
 
     public Integer idMostExpansiveSale( FileDat fileDat ) {
-        return  serviceSale.mostExpansiveSale( fileDat.getSales() );
+        return  SaleService.mostExpansiveSale( fileDat.getSales() );
     }
 
     public String worstSeller( FileDat fileDat ) {
-        return serviceSale.worstSeller( fileDat.getSales() );
+        return SaleService.worstSeller( fileDat.getSales() );
     }
  }
