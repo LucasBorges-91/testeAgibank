6748ae294cfecc03b382bc5e061624a23c46877b
Lucas Borges <lucas.pborges@yahoo.com.br>
2020-08-04 10:38:14 -0300

    add log createDirectory

--- src/main/java/br/com/testagibank/testeAgibank/util/CreateDirectory.java
+++ src/main/java/br/com/testagibank/testeAgibank/util/CreateDirectory.java
@@ -1,28 +1,41 @@
 package br.com.testagibank.testeAgibank.util;
 
 import java.io.File;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 
 public class CreateDirectory {
+
+    private static final Logger LOGGER = Logger.getLogger( CreateDirectory.class.getName() );
+
 
     public static void createAllDirectory( String directory ) {
 
         File dir = new File( System.getProperty("user.home") + "/" + directory );
         if ( !dir.exists() ) {
             dir.mkdir();
+            LOGGER.log( Level.INFO, "Directory created "
+                    + System.getProperty("user.home") + "/" + directory );
         }
 
-        File fileIn = new File( System.getProperty("user.home") + "/" + directory + "/in/" );
+        File fileIn = new File( dir.getPath() + "/in/" );
         if ( !fileIn.exists() ) {
             fileIn.mkdir();
+            LOGGER.log( Level.INFO, "Directory created "
+                    + dir.getPath() + "/in/" );
         }
-        File fileOut = new File (fileIn.getParent() + "/out/" );
+        File fileOut = new File (dir.getPath() + "/out/" );
         if ( !fileOut.exists() ) {
             fileOut.mkdir();
+            LOGGER.log( Level.INFO, "Directory created "
+                    + dir.getPath() + "/out/" );
         }
 
-        File fileBackup = new File( fileIn.getParent() + "/backup/" );
+        File fileBackup = new File( dir.getPath() + "/backup/" );
         if ( !fileBackup.exists() ) {
             fileBackup.mkdir();
+            LOGGER.log( Level.INFO, "Directory created "
+                    + dir.getPath() + "/backup/" );
         }
     }
 }
