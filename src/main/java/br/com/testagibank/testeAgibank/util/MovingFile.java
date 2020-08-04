5d30fb8b3fbd13a69198cbcfe5c37d408b57e146
Lucas Borges <lucas.pborges@yahoo.com.br>
2020-08-04 10:37:52 -0300

    add log in movingFile

--- src/main/java/br/com/testagibank/testeAgibank/util/MovingFile.java
+++ src/main/java/br/com/testagibank/testeAgibank/util/MovingFile.java
@@ -1,13 +1,21 @@
 package br.com.testagibank.testeAgibank.util;
 
 import java.io.File;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 
 public class MovingFile {
+
+    private static final Logger LOGGER = Logger.getLogger( MovingFile.class.getName() );
 
     public static Boolean movingFile(File file ) {
         File source = new File( file.getPath() );
         File dest = new File( System.getProperty("user.home")
                 + "/data/backup/" + file.getName() );
+
+        if (!source.renameTo(dest)) {
+            LOGGER.log(Level.WARNING, "Error to moving file");
+        }
         return source.renameTo( dest );
     }
 }
