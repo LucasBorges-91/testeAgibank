2e2e1594d9332a2502ec2fac1a8126cecf90f6ab
Lucas Borges <lucas.pborges@yahoo.com.br>
2020-08-04 10:37:33 -0300

    add log in readFile

--- src/main/java/br/com/testagibank/testeAgibank/util/ReadFile.java
+++ src/main/java/br/com/testagibank/testeAgibank/util/ReadFile.java
@@ -8,10 +8,15 @@ import java.io.FileReader;
 import java.io.IOException;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 
 public class ReadFile {
 
+    private static final Logger LOGGER = Logger.getLogger( ReadFile.class.getName() );
+
     public static List<String> readFile( File file ) {
+
         List<String> lines = new ArrayList<>();
         if ( ValidateExtension.isDAT( file ) ) {
             try ( BufferedReader br = new BufferedReader( new FileReader( file.getPath() ) ) ) {
@@ -26,10 +31,10 @@ public class ReadFile {
                     }
                 }
             }catch ( IOException e ) {
-                System.out.println( "Error reading file: " + e.getMessage() );
+                LOGGER.log( Level.SEVERE, "Error reading file: " + e.getMessage() );
             }
         } else {
-            throw new ExtensionException( "Extension invalid format." );
+            LOGGER.log( Level.WARNING, new ExtensionException( "Extension invalid format." ).getMessage() );
         }
         return lines;
     }
